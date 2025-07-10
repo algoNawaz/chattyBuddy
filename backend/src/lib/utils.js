b660084af7636379c2f53855d68e4d139bad1564
shahnawaz3141 <shahnawaz3439@gmail.com>
2025-07-10 15:31:23 +0530

    Initial commit

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,16 @@
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userId,res) => {
+    const token = jwt.sign({userId},process.env.JWT_SECRET,{
+        expiresIn:"7d",
+    });
+
+    res.cookie("jwt",token,{
+        maxAge: 7*24*60*60*1000,  // milisec
+        httpOnly: true , //prevent XSS attack cross-site-scripting
+        sameSite: "strict", //CSRF attack cross-site request forgery attack
+        secure: process.env.NODE_ENV !=="development",
+    });
+
+    return token;
+};

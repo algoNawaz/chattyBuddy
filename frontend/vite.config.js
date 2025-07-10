7c1612dd0ad1c443c461aa0e284d06bae5f5a0f5
shahnawaz3141 <shahnawaz3439@gmail.com>
2025-07-10 16:24:00 +0530

    5

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // should be './frontend' ONLY if vite.config.js is in the root
-});
+  // DO NOT use "root" or "build.outDir"
+})
